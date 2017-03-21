622faf0c56b10a0b097d634fd91fdb14c19abc17
Brian <369429428@qq.com>
2017-03-21 08:50:17 +0800

    update

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: path.join(path.resolve(__dirname, '../dist'), '/'),
+    assetsPublicPath: 'https://gobriango.github.io/chatroom/dist/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
